af1eb2a53a1df7d458d56961dee5b5744c2bdcef
silver-blinder <xluo8105@gmail.com>
2024-06-04 17:13:43 +0800

    third commit

--- src/conponent/qr-code/index.jsx
+++ src/conponent/qr-code/index.jsx
@@ -4,14 +4,14 @@ const QRcode = () => {
   return (
     <div>
       <div className="qr-container">
-        <img src="src/images/image-qr-code.png" alt="qr-code" />
+        <img src="/static/images/image-qr-code.png" alt="qr-code" />
         <div className="production-1">
           <p>Improve your front-end skills by building projects</p>
         </div>
         <div className="production-2">
           <p>
             Scan the QR code to visit Frontend Mentor and take your coding
-            skills to the next level
+            skills to the next `level
           </p>
         </div>
       </div>
